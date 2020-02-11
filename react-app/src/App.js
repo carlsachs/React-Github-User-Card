06c9e5cc79d60555687435d9ad508b948bcecb01
carlsachs <sachscarl@gmail.com>
2020-02-11 15:54:19 -0500

    much progress on today's project

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Card from "./components/Card.js";
 
 class App extends React.Component {
   state = {
     user: "",
     setUser: ""
   }
+
+  componentDidMount() {
+    Axios.get('https://api.github.com/users/carlsachs')
+    .then(res => {
+      console.log(res);
+      this.setState({
+        user: res.data.login
+      });
+    })
+    .catch(err => {
+      console.log("There's an error", err)
+    });
+
+  }
   render() {
   return (
     <div className="App">
-      
+      <h1>GitHub User Card</h1>
+      <Card />
     </div>
   );
 }
---
+++ react-app/src/components/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardMain = styled.div`
+    background-color: lightgreen;
+    width: 50%;
+    margin: 0 auto;
+    margin-top: 5%;
+    h2 {
+        color: forestgreen;
+    }
+    h3{
+        color: forestgreen;
+    }
+    padding-bottom: 20px;
+    padding-top: 20px;
+    box-shadow: 8px 5px 5px darkgreen;
+
+`;
+
+class Card extends React.Component {
+
+    render() {
+        return (
+            <CardMain>
+                <h2>this.state.login</h2>
+                <a href src="this.state.html_url">Link to Github</a>
+                <h3>Repos:</h3>
+                <h3>this.state.repos_url</h3>
+                <h3>Followers: this.state.followers_url</h3>
+            </CardMain>
+        )
+    }
+}
+export default Card;
